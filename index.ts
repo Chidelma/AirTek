7c68a0167ed6fba34b4c76216e05b71e9689dd2a
chidelma <samuel@clickspace.com>
2023-06-12 18:46:10 -0400

    added port 3000 to security group

--- index.ts
+++ index.ts
@@ -1,45 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as awsx from '@pulumi/awsx'
 
 const project_name = 'infra-team'
 
 const tags = { 
   Name: `${project_name}`
 }
-
-const vpc = new awsx.ec2.Vpc(`${project_name}-vpc`, {
-  natGateways: {
-    strategy: awsx.ec2.NatGatewayStrategy.OnePerAz,
-  },
-  tags: tags
-});
-
-const cluster = new aws.ecs.Cluster(`${project_name}-cluster`, {tags: tags});
-
-const group = new aws.ec2.SecurityGroup(`${project_name}-sg`, {
-  vpcId: vpc.vpcId,
-  description: "Enable HTTP access",
-  ingress: [{
-    protocol: "tcp",
-    fromPort: 80,
-    toPort: 80,
-    cidrBlocks: ["0.0.0.0/0"],
-  }],
-  egress: [{
-    protocol: "-1",
-    fromPort: 0,
-    toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],
-  }],
-  tags: tags
-});
-
-const alb = new awsx.lb.ApplicationLoadBalancer(`${project_name}-lb`, {
-  securityGroups: [group.id],
-  subnetIds: vpc.publicSubnetIds,
-  tags: tags
-});
 
 // Create a repository to store image for web
 const repoWeb = new awsx.ecr.Repository(`${project_name}-web`, {
@@ -63,13 +30,53 @@ const imageApi = new awsx.ecr.Image(`${project_name}-api`, {
     path: './infra-api'
 })
 
-const taskDefinition = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, {
+const vpc = new awsx.ec2.Vpc(`${project_name}-vpc`, {
+  cidrBlock: "10.0.0.0/16",
+  tags: tags,
+  instanceTenancy: "default"
+});
+
+const sg = new aws.ec2.SecurityGroup(`${project_name}-sg`, {
+    vpcId: vpc.vpcId,
+    ingress: [{
+        fromPort: 80,
+        toPort: 80,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"]
+    },
+    {
+      fromPort: 3000,
+      toPort: 3000,
+      protocol: "tcp",
+      cidrBlocks: ["0.0.0.0/0"]
+    }],
+    egress: [{
+        fromPort: 0,
+        toPort: 0,
+        protocol: "-1",
+        cidrBlocks: ["0.0.0.0/0"]
+    }],
+    tags: tags
+});
+
+// Create an ECS cluster
+const cluster = new aws.ecs.Cluster(`${project_name}-cluster`, {
+  tags: tags
+})
+
+const lb = new awsx.lb.ApplicationLoadBalancer(`${project_name}-lb`, {
+  tags: tags,
+  securityGroups: [sg.id],
+  subnetIds: vpc.publicSubnetIds
+})
+
+const td = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, {
   tags: tags,
   containers: {
     infraweb: {
       image: imageWeb.imageUri,
-      memory: 1024,
-      cpu: 2048,
+      memory: 128,
+      cpu: 512,
       essential: true,
       environment: [{
         name: 'ApiAddress',
@@ -84,8 +91,8 @@ const taskDefinition = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, 
     },
     infraapi: {
       image: imageApi.imageUri,
-      memory: 1024,
-      cpu: 2048,
+      memory: 128,
+      cpu: 512,
       essential: true,
       portMappings: [
         {
@@ -95,23 +102,24 @@ const taskDefinition = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, 
       ],
     }
   }
-});
+})
 
-const service = new awsx.ecs.FargateService(`${project_name}-svc`, {
-  cluster: cluster.arn,
-  desiredCount: 2,
-  taskDefinition: taskDefinition.taskDefinition.arn,
+// Lets use a serverless service (Fargate) from Elastic Container Service
+const service =  new awsx.ecs.FargateService(`${project_name}-service`, {
   networkConfiguration: {
     assignPublicIp: true,
     subnets: vpc.privateSubnetIds,
-    securityGroups: [group.id],
+    securityGroups: [sg.id]
   },
+  cluster: cluster.arn,
+  desiredCount: 2,
+  taskDefinition: td.taskDefinition.arn,
+  tags: tags,
   loadBalancers: [{
-    targetGroupArn: alb.defaultTargetGroup.arn,
-    containerName: "infraweb",
-    containerPort: 3000,
-  }],
-  tags: tags
-});
+    targetGroupArn: lb.defaultTargetGroup.arn,
+    containerName: 'infraweb',
+    containerPort: 3000
+  }]
+})
 
-export const url = pulumi.interpolate`http://${alb.loadBalancer.dnsName}`;
+export const ecsTaskUrl = pulumi.interpolate`http://${lb.loadBalancer.dnsName}`
