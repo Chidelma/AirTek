4f51e1ff1f5c7981e8a9d75e0005107f604cae0c
chidelma <samuel@clickspace.com>
2023-06-14 18:38:22 -0400

    refined networking

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import * as awsx from '@pulumi/awsx'
 
 const project_name = 'infra-team'
 
+const path = '/WeatherForecast'
+
 const tags = { 
   Name: `${project_name}`
 }
@@ -33,23 +35,25 @@ const imageApi = new awsx.ecr.Image(`${project_name}-api`, {
 const vpc = new awsx.ec2.Vpc(`${project_name}-vpc`, {
   cidrBlock: "10.0.0.0/16",
   tags: tags,
-  instanceTenancy: "default"
+  instanceTenancy: "default",
 });
 
 const sg = new aws.ec2.SecurityGroup(`${project_name}-sg`, {
     vpcId: vpc.vpcId,
-    ingress: [{
+    ingress: [
+      {
+        fromPort: 5000,
+        toPort: 5000,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"]
+      },
+      {
         fromPort: 80,
         toPort: 80,
         protocol: "tcp",
         cidrBlocks: ["0.0.0.0/0"]
-    },
-    {
-      fromPort: 3000,
-      toPort: 3000,
-      protocol: "tcp",
-      cidrBlocks: ["0.0.0.0/0"]
-    }],
+      }
+    ],
     egress: [{
         fromPort: 0,
         toPort: 0,
@@ -64,36 +68,52 @@ const cluster = new aws.ecs.Cluster(`${project_name}-cluster`, {
   tags: tags
 })
 
-const lb = new awsx.lb.ApplicationLoadBalancer(`${project_name}-lb`, {
+const web_lb = new awsx.lb.ApplicationLoadBalancer(`${project_name}-web-lb`, {
   tags: tags,
   securityGroups: [sg.id],
   subnetIds: vpc.publicSubnetIds
 })
 
-const td = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, {
+const api_lb = new awsx.lb.ApplicationLoadBalancer(`${project_name}-api-lb`, {
+  tags: tags,
+  internal: true,
+  securityGroups: [sg.id],
+  subnetIds: vpc.privateSubnetIds,
+  defaultTargetGroup: {
+    healthCheck: {
+      path: path
+    }
+  }
+})
+
+const web_td = new awsx.ecs.FargateTaskDefinition(`${project_name}-web-td`, {
   tags: tags,
   containers: {
     infraweb: {
       image: imageWeb.imageUri,
       memory: 128,
       cpu: 512,
-      essential: true,
       environment: [{
         name: 'ApiAddress',
-        value: `http://127.0.0.1:5000/WeatherForecast`
+        value: pulumi.interpolate`http://${api_lb.loadBalancer.dnsName}${path}`
       }],
       portMappings: [
         {
-          containerPort: 3000,
-          hostPort: 3000
+          containerPort: 5000,
+          hostPort: 5000
         }
       ],
-    },
+    }
+  }
+})
+
+const api_td = new awsx.ecs.FargateTaskDefinition(`${project_name}-api-td`, {
+  tags: tags,
+  containers: {
     infraapi: {
       image: imageApi.imageUri,
       memory: 128,
       cpu: 512,
-      essential: true,
       portMappings: [
         {
           containerPort: 5000,
@@ -105,21 +125,38 @@ const td = new awsx.ecs.FargateTaskDefinition(`${project_name}-td`, {
 })
 
 // Lets use a serverless service (Fargate) from Elastic Container Service
-const service =  new awsx.ecs.FargateService(`${project_name}-service`, {
+const web_srv = new awsx.ecs.FargateService(`${project_name}-web-srv`, {
   networkConfiguration: {
     assignPublicIp: true,
-    subnets: vpc.privateSubnetIds,
+    subnets: vpc.publicSubnetIds,
     securityGroups: [sg.id]
   },
   cluster: cluster.arn,
   desiredCount: 2,
-  taskDefinition: td.taskDefinition.arn,
+  taskDefinition: web_td.taskDefinition.arn,
   tags: tags,
   loadBalancers: [{
-    targetGroupArn: lb.defaultTargetGroup.arn,
+    targetGroupArn: web_lb.defaultTargetGroup.arn,
     containerName: 'infraweb',
-    containerPort: 3000
+    containerPort: 5000
   }]
 })
 
-export const ecsTaskUrl = pulumi.interpolate`http://${lb.loadBalancer.dnsName}`
+const api_srv = new awsx.ecs.FargateService(`${project_name}-api-srv`, {
+  networkConfiguration: {
+    subnets: vpc.privateSubnetIds,
+    securityGroups: [sg.id],
+    assignPublicIp: false
+  },
+  cluster: cluster.arn,
+  desiredCount: 2,
+  taskDefinition: api_td.taskDefinition.arn,
+  tags: tags,
+  loadBalancers: [{
+    targetGroupArn: api_lb.defaultTargetGroup.arn,
+    containerName: 'infraapi',
+    containerPort: 5000
+  }]
+})
+
+export const ecsTaskUrl = pulumi.interpolate`http://${web_lb.loadBalancer.dnsName}`
